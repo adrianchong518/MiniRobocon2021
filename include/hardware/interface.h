a2b3155a1ee1407c12bf335172911517493039b5
Adrian Chong <adrianchong518@gmail.com>
2021-05-29 13:07:54 +0800

    Added colorized outputs

--- include/hardware/interface.h
+++ include/hardware/interface.h
@@ -3,13 +3,15 @@
 #include <Arduino.h>
 
 #if DEBUG == 1
-#define LOG_DEBUG(msg) Serial.println("[DEBUG]\t" + String(msg))
+#define LOG_DEBUG(msg) \
+  Serial.println("\e[;90m[DEBUG]\t" + String(msg) + "\e[0m")
 #else
 #define LOG_DEBUG(msg)
 #endif
 
 #define LOG_INFO(msg) Serial.println("[INFO]\t" + String(msg))
-#define LOG_ERROR(msg) Serial.println("[ERROR]\t" + String(msg))
+#define LOG_ERROR(msg) \
+  Serial.println("\e[1;91m[ERROR]\t" + String(msg) + "\e[0m")
 
 namespace hardware {
 namespace interface {}  // namespace interface
