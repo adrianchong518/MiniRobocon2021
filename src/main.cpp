755b51e3605102ec8cfc5f667a48069f0a8b1f77
adrianchong518 <adrianchong518@gmail.com>
2022-05-18 15:26:46 +0800

    Emptys project

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,7 @@
 #include <Arduino.h>
-#include <Wire.h>
 
-#include "constants.h"
-#include "hardware/hardware.h"
-#include "control/control.h"
-#include "utils/time.h"
+#include <PID.h>
 
-// #define IS_TIMING
+void setup() { Serial.begin(115200); }
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin();
-
-  hardware::init();
-
-  hardware::calibrate();
-
-  hardware::startingPosition();
-
-  control::init();
-
-  LOG_INFO("<Main>\t\tReady");
-  hardware::interface::lcd.setCursor(1, 3);
-  hardware::interface::lcd.print("  Main Loop  ");
-}
-
-#ifdef IS_TIMING
-unsigned long start, end;
-void loop() {
-  start = micros();
-  time::preLoop();
-  hardware::loop();
-  control::loop();
-  time::postLoop();
-  end = micros();
-  LOG_INFO("<Main>\t\tTime Taken:\t" + String(end - start) + "\tus");
-}
-#else
-void loop() {
-  time::preLoop();
-  hardware::loop();
-  control::loop();
-  time::postLoop();
-}
-#endif
+void loop() {}
